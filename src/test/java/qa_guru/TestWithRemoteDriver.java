7dff6d1f8ec8811faa620e7d3dcc09958de89eff
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-23 20:10:32 +0500

    added steps

--- src/test/java/qa_guru/TestWithRemoteDriver.java
+++ src/test/java/qa_guru/TestWithRemoteDriver.java
@@ -5,14 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 @Tag("remote_test")
 public class TestWithRemoteDriver extends TestBase{
     @Test
     void searchSelenideInYandex(){
-        open("https://ya.ru/");
-        $("#text").click();
-        $("#text").setValue("Selenide");
-        $("button[type='submit']").click();
+        step("Открываем сайт Яндекса", ()->{
+            open("https://ya.ru/");
+        });
+
+        step("Кликаем по полю ввода", ()->{
+            $("#text").click();
+        });
+
+        step("В поле ввода вводим поисковой запрос", ()->{
+            $("#text").setValue("Selenide");
+        });
+
+        step("Кликаем по кнопке поиска", ()->{
+            $("button[type='submit']").click();
+        });
     }
 }
