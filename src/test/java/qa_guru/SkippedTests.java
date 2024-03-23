f6e8a80b99d06d453426c75b304e752da886350b
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-23 14:54:18 +0500

    init commit

---
+++ src/test/java/qa_guru/SkippedTests.java
@@ -0,0 +1,28 @@
+package qa_guru;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test1(){
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void test2(){
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void test3(){
+        assertTrue(false);
+    }
+}
