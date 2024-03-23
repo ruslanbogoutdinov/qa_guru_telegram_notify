ab7b674d410e93a5c793c95448dbd7e9b7af64e8
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-23 19:59:54 +0500

    fix allure result for remote_test

--- src/test/java/qa_guru/TestWithRemoteDriver.java
+++ src/test/java/qa_guru/TestWithRemoteDriver.java
@@ -1,10 +1,12 @@
 package qa_guru;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+@Tag("remote_test")
 public class TestWithRemoteDriver extends TestBase{
     @Test
     void searchSelenideInYandex(){
