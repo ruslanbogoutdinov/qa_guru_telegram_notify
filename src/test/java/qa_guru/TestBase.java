f41f2b587603cdaf8b585f0aa7b52206583d0504
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-25 23:37:29 +0500

    some remote fixes

--- src/test/java/qa_guru/TestBase.java
+++ src/test/java/qa_guru/TestBase.java
@@ -14,12 +14,14 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll(){
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100";
+        //Configuration.browserSize = "1920x1080";
+        //Configuration.browser = "chrome";
+        //Configuration.browserVersion = "100";
+
         // настройка для remote запуска
         // прописываем также логин и пароль в начале
         // при запуске данного теста, локальный браузер не должен запускаться
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
