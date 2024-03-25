fb47e20a0541e5cf8e71b6f47110e0c43d45f757
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-25 20:08:04 +0500

    added properties

---
+++ src/test/java/qa_guru/properties/SystemPropertiesTests.java
@@ -0,0 +1,25 @@
+package qa_guru.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class SystemPropertiesTests {
+    @Test
+    @Tag("property_test")
+    void simplePropertiesTest(){
+        // устанавливаем значение
+        //System.setProperty("browser", "opera");
+        // считываем значение
+        String browserName = System.getProperty("browser", "mozilla");
+        String browserVersion = System.getProperty("browser_version", "99");
+
+        System.out.println(browserName);
+        System.out.println(browserVersion);
+
+        // пример с property
+        // System.setProperty("user_name", "123");
+        System.out.println(format("Hello, %s!", System.getProperty("user_name", "unknown user")));
+    }
+}
