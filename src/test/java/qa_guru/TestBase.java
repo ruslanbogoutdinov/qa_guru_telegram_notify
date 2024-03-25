3a3b54c57475ef439747d1a42c46285f45c1354b
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-26 00:54:55 +0500

    some changes v3

--- src/test/java/qa_guru/TestBase.java
+++ src/test/java/qa_guru/TestBase.java
@@ -18,10 +18,10 @@ public class TestBase {
         // прописываем также логин и пароль в начале
         // при запуске данного теста, локальный браузер не должен запускаться
 
+        Configuration.baseUrl = System.getProperty("baseUrl");
         Configuration.browser = System.getProperty("browser");
         Configuration.browserSize = System.getProperty("browserSize");
         Configuration.browserVersion = System.getProperty("browserVersion");
-        Configuration.baseUrl = System.getProperty("baseUrl");
         //Configuration.remote = "https://user1:1234@"+System.getProperty("selenoidUrl", "selenoid.autotests.cloud/wd/hub");
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
