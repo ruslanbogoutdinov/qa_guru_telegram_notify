6d57579e05899f6a9f8408f7d5c2fee27638f8b6
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-26 00:41:28 +0500

    some changes v3

--- src/test/java/qa_guru/TestWithRemoteDriver.java
+++ src/test/java/qa_guru/TestWithRemoteDriver.java
@@ -3,8 +3,9 @@ package qa_guru;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 //@Tag("remote_test")
@@ -12,7 +13,12 @@ import static io.qameta.allure.Allure.step;
 public class TestWithRemoteDriver extends TestBase{
     @Test
     void searchSelenideInYandex(){
-            step("Открываем сайт Яндекс", ()->{
+        open("https://github.com/");
+
+        $$(".HeaderMenu-item").get(1).hover();
+        $("[aria-labelledby=solutions-for-heading]").$(byText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+        /*step("Открываем сайт Яндекс", ()->{
             open("https://ya.ru/");
         });
 
@@ -26,6 +32,6 @@ public class TestWithRemoteDriver extends TestBase{
 
         step("Кликаем по кнопке поиска", ()->{
             $("button[type='submit']").click();
-        });
+        });*/
     }
 }
