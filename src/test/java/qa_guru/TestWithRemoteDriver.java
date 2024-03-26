8798170fe8a8e0121bb27dfcb0775377383fed8c
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-26 22:02:12 +0500

    added fix #1

--- src/test/java/qa_guru/TestWithRemoteDriver.java
+++ src/test/java/qa_guru/TestWithRemoteDriver.java
@@ -4,15 +4,15 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 @Tag("remote_test")
 public class TestWithRemoteDriver extends TestBase{
     @Test
     void searchSelenideInYandex(){
-        Configuration.timeout = 10000;
         open("https://github.com/");
+
+        sleep(5000);
 
         $$(".HeaderMenu-item").get(1).hover();
     }
